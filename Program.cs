731cc28c711ebaa253f1b92677a10a6b1b93d23f
Vinicius Zuba <vinnydisorderofficial@gmail.com>
2025-04-17 19:48:01 +0200

    commit do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n= new Nokia("111111","modelete tijolo","5445454",8);
+n.Ligar();
+n.InstalarAplicativo("orkut");
+
+Iphone ph= new Iphone("2222222","modelete apple","5546666",32);
+ph.Ligar();
+ph.InstalarAplicativo("whatsapp");
